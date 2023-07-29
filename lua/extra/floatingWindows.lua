4abd600c13fdd2f07d69c2eb78f3fef22bb3a4e5
gruescuipatiedaniel <gruescu.daniel@gmail.com>
2023-07-29 19:11:50 +0300

    V1.1.0 Added bufferUtilies and moved the needed functions there

--- lua/extra/floatingWindows.lua
+++ lua/extra/floatingWindows.lua
@@ -1,8 +1,4 @@
-local function insert_data_into_buffer(buffer, data)
-    local lines = vim.split(data, "\n", true)
-    vim.api.nvim_buf_set_lines(buffer, 0, -1, false, lines)
-end
-
+require("utilities.bufferUtilities")
 
 function summonScratchPad()
     width = 50
@@ -29,24 +25,7 @@ function summonScratchPad()
 end
 
 
-local function get_data_from_buffer(bufnr)
-    local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-    local buffer_content = table.concat(lines, "\n")
-    return buffer_content
-end
-     
-
-local function close_buffer_by_name(buffer_name)
-    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-        local name = vim.api.nvim_buf_get_name(bufnr)
-        if name == buffer_name or name == "" then
-            if name == buffer_name then
-               _G.buffer_content_variable = get_data_from_buffer(bufnr)
-            end
-            vim.api.nvim_buf_delete(bufnr, { force = true })
-        end
-    end
-end
+    
 
 function scratchPad()
     local success, err = pcall(summonScratchPad)
